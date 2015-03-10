85dccb93910ce7f31461699ff624fa97c6b71e79
Liaxo <meilia14@student.westerdals.no>
2015-03-10 16:26:04 +0100

    page1+

--- singlepage/page1.php
+++ singlepage/page1.php
@@ -1,25 +1,30 @@
 <?php require_once 'header.php';?>
 <img src="../images/textlogo.png" id="textlogo">
 
-<div id="innloggingwrapper">
-	<ul>	
-		<li class="innloggingsliste">
-			<p class="bruker">Brukernavn</p>
-			<div class="brukerdiv">
-				<input class="brukerinput" type="text" name="email" required title="Skriv inn e-post!">
-			</div>
-		</li>
-		<li class="innloggingsliste">
-			<p class="bruker">Passord</p>
-			<div class="brukerdiv">
-				<input class="brukerinput" type="password" name="password" required title="Skriv inn passord!">
-			</div>
-		</li>
-		<li class="innloggingsliste">
-			<input id="submit" type="submit" value="Logg inn" name="logginn">
-		</li>
-	</ul>
-</div>
+<form id="innloggingsform" action="" method="post" name="form">
+	<div id="innloggingwrapper">
+		<ul>	
+			<li class="innloggingsliste">
+				<p class="bruker">Brukernavn*</p>
+				<div class="brukerdiv">
+					<input class="brukerinput" type="text" name="email" required title="Skriv inn e-post!">
+				</div>
+			</li>
+			<li class="innloggingsliste">
+				<p class="bruker">Passord*</p>
+				<div class="brukerdiv">
+					<input class="brukerinput" type="password" name="password" required title="Skriv inn passord!">
+				</div>
+			</li>
+			<li class="innloggingsliste">
+				<div id="submitdiv">
+					<input id="submit" type="submit" value="Logg inn" name="logginn">
+				</div>
+			</li>
+		</ul>
+	</div>
+</form>
 
+<div id="page1bot"><div> 
 <?php require_once 'footer.php';?>
 
