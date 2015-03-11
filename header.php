530cac78246efbdfe2fde22106227ee3c9df4da6
morkeseth <morkeseth@Patriks-MacBook-Pro-2.local>
2015-03-11 12:37:47 +0100

    fikset responsitvt design

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 		<meta charset="UTF-8">
 		<title>Grupperom</title>
 		<link rel="stylesheet" type="text/css" href="css/css.css">
+		<link rel="stylesheet" type="text/css" href="css/mediaqueries.css">
 		<style type="text/css">
 			<?php 
 			$box = array('#835581', '#FED575', '#1EB6EC', '#FD7265', '#63CAA6');
