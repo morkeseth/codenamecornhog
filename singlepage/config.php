320e190217139bca5ed0a7b563b89e7d455c7fc7
Liaxo <meilia14@student.westerdals.no>
2015-03-11 11:14:31 +0100

    believe robot

--- singlepage/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$db_host = "123.0.0.1";
-$db_name = "pj2100";
-$db_user = "root";
-$db_pass = "";
--- singlepage/footer.php
+++
@@ -1,9 +0,0 @@
-		<footer>
-			<img id="footerpic" src ="../images/footer.png">
-			<img class="mainlogo" src ="../images/mainlogo.png">
-		</footer>
-
-
-	</body>
-
-</html>
