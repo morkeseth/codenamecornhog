11270804c09a6336b55058cd3ee81760941c7b34
morkeseth <morkeseth@Patriks-MacBook-Pro-2.local>
2015-03-11 13:02:11 +0100

    no zoom mobile

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Grupperom</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
 		<link rel="stylesheet" type="text/css" href="css/css.css">
 		<link rel="stylesheet" type="text/css" href="css/mediaqueries.css">
 		<style type="text/css">
